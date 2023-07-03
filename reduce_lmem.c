da1ffb31b1fb14183f2e22e78a105aabd91d0054
Xernary <nicolalggiuffrida@gmail.com>
2023-07-03 16:19:11 +0200

    idk

--- reduce_lmem.c
+++ reduce_lmem.c
@@ -34,7 +34,7 @@ cl_event init_array(cl_kernel kernel, cl_command_queue que, int nels,
   return evt;
 }
 
-cl_event reduce(cl_kernel kernel, cl_command_queue que, int nels,
+cl_event reduce_lmem(cl_kernel kernel, cl_command_queue que, int nels,
                     int lws_arg, int preferred_lws, 
                     cl_mem d_input, cl_mem d_output, cl_event init_evt){
 
@@ -52,6 +52,9 @@ cl_event reduce(cl_kernel kernel, cl_command_queue que, int nels,
 
   err = clSetKernelArg(kernel, 2, sizeof(d_output), &d_output);
   ocl_check(err, "set reduce kernel arg");
+
+  err = clSetKernelArg(kernel, 3, sizeof(cl_int) * lws[0], NULL);
+  ocl_check(err, "set lmem kernel arg");
 
   err = clEnqueueNDRangeKernel(que, kernel, 1, NULL, gws, lws, 1, 
                                &init_evt, &evt);
@@ -98,7 +101,7 @@ int main(int argn, char* args[]){
   cl_kernel init_kernel = clCreateKernel(prog, "init_array", &err);
   ocl_check(err, "create init kernel");
 
-  cl_kernel reduce_kernel_v1 = clCreateKernel(prog, "reduce_v1", &err);
+  cl_kernel reduce_kernel_lmem = clCreateKernel(prog, "reduce_lmem", &err);
   ocl_check(err, "create reduce kernel");
 
   // allocate input/output device buffers
@@ -120,37 +123,26 @@ int main(int argn, char* args[]){
                                  &preferred_init_lws, NULL);
   ocl_check(err, "get preferred init lws");
 
-  size_t preferred_reduce_v1_lws;
-  err = clGetKernelWorkGroupInfo(reduce_kernel_v1, d, 
+  size_t preferred_reduce_lmem_lws;
+  err = clGetKernelWorkGroupInfo(reduce_kernel_lmem, d, 
                           CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
-                                 sizeof(preferred_reduce_v1_lws), 
-                                 &preferred_reduce_v1_lws, NULL);
+                                 sizeof(preferred_reduce_lmem_lws), 
+                                 &preferred_reduce_lmem_lws, NULL);
   ocl_check(err, "get preferred reduce lws");
 
   // launch kernels
   cl_event init_evt = init_array(init_kernel, que, nels, lws, 
                                  preferred_init_lws, d_input);
-  int steps = log(nels)/log(4);
-  cl_event reduce_evts[steps + 1];
-  int current_nels = nels;
-  reduce_evts[0] = init_evt;
-  for(int i = 1; i <= steps; i++){
 
-    reduce_evts[i] = reduce(reduce_kernel_v1, que, current_nels/4, lws,
-                            preferred_reduce_v1_lws, d_input,
-                            d_output, reduce_evts[i-1]);
-    cl_mem temp = d_input;
-    d_input = d_output;
-    d_output = temp;
-    current_nels /= 4;
-  }
+  cl_event reduce_evt = reduce_lmem(reduce_kernel_lmem, que, nels/4, 
+                                    lws, preferred_reduce_lmem_lws, 
+                                    d_input, d_output, init_evt);
 
-  printf("final nels obtained: %d, steps: %d\n", current_nels, steps);
   cl_int h_result;
   cl_event read_evt;
-  err = clEnqueueReadBuffer(que, d_input, CL_TRUE, 0, 
+  err = clEnqueueReadBuffer(que, d_output, CL_TRUE, 0, 
                                       sizeof(cl_int), &h_result, 1, 
-                                      &reduce_evts[steps], &read_evt);
+                                      &reduce_evt, &read_evt);
   ocl_check(err, "read buff");
   printf("RESULT: %d\n", h_result);
   verify(nels, h_result);
@@ -163,20 +155,18 @@ int main(int argn, char* args[]){
   double reduce_time = 0;
   double read_time = runtime_ms(read_evt);
 
-  for(int i = 1; i <= steps; i++){
-    printf("reduce[%d]: %lf\n", i, runtime_ms(reduce_evts[i]));
-    reduce_time += runtime_ms(reduce_evts[i]); 
-  }
-
   printf("init time: %lf\n", init_time);
-  printf("reduce time: %lf\n", reduce_time);
+//  printf("reduce time: %lf\n", reduce_time);
   printf("read time: %lf\n", read_time);
+  
+  // illegal
+
 
   // release boiler
   clReleaseMemObject(d_input);
   clReleaseMemObject(d_output);
   clReleaseKernel(init_kernel);
-  clReleaseKernel(reduce_kernel_v1);
+  clReleaseKernel(reduce_kernel_lmem);
   clReleaseProgram(prog);
   clReleaseCommandQueue(que);
   clReleaseContext(ctx);
